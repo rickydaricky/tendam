d7a5b51120763686c06594539df8e787bfa1bee3
heon01px2020 <heon01px2020@gmail.com>
2021-04-08 04:18:51 +0900

    added blackjack file

---
+++ blackjack/src/BlackJack.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
+import TextBox from "./TextBox";
+
+function BlackJack(props) {
+    
+
+
+    return (
+        <div>
+            Testing hello!
+            {/* <TextBox label="" change={testIng} /> */}
+            {/* <AwesomeButton type="primary" onPress={requestStreetRoute}> Manual Street Input Button </AwesomeButton> */}
+        </div>
+    )
+}
+
+/**
+ * Gets key longitude
+ * @param lon
+ * @param lambda
+ * @returns {number}
+ */
+//  function convertKeytoCard(lon, lambda) {
+//     return Math.floor(lon / lambda) * lambda;
+// }
+
+export default BlackJack
